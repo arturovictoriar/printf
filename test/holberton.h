44eaa9d8ff712711104b2f0beb59b5098c8c65bb
arvicrin <1231@holbertonschool.com>
2019-10-27 23:30:32 +0000

    New

---
+++ test/holberton.h
@@ -0,0 +1,29 @@
+#ifndef _PRINTF_LIB
+#define _PRINTF_LIB
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+  * struct op - Struct op
+  *
+  * @op: The operator
+  * @f: The function associated
+  */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list);
+} op_t;
+
+int _printf(const char *format, ...);
+int st(va_list a);
+int ch(va_list a);
+int por(va_list a);
+int inte(va_list a);
+
+#endif
