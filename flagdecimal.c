5efc139d3fd3e4297008996d47aa32a5c9187d60
arvicrin <1231@holbertonschool.com>
2019-10-27 05:44:11 +0000

    Betty: OK, Compilation: OK, Comments: Can be better, Conditions: the name of the function can be upside down

---
+++ flagdecimal.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+
+/**
+* inte - fills memory with a constant byte
+* @a: the value to print
+* Return: numbers of characters printed
+*/
+int inte(va_list a)
+{
+	int num = va_arg(a, int), i, j, cant = 0, dig = 0, num1 = 0, neg = 0;
+	char *number;
+
+	num1 = num;
+	if (num1 < 0)
+	{
+		num1 *= -1;
+		num *= -1;
+		neg = 1;
+	}
+
+	for (i = 1; num1 / 10 != 0; i++)
+		num1 /= 10;
+
+	number = malloc(sizeof(char) * (i + 1 + neg));
+	if (number == NULL)
+		return (0);
+	for (j = 0; j < i; j++)
+	{
+		dig = num % 10;
+		number[(i + neg - 1) - j] = dig + '0';
+		num /= 10;
+	}
+	if (neg == 1)
+		number[0] = '-';
+	number[j + neg] = '\0';
+	cant = write(1, number, i + neg);
+	free(number);
+	return (cant);
+}
+
+/**
+* deci - fills memory with a constant byte
+* @a: the value to print
+* Return: numbers of characters printed
+*/
+int deci(va_list a)
+{
+	int num = va_arg(a, int), i, j, cant = 0, dig = 0, num1 = 0;
+	char *number;
+
+	num1 = num;
+	for (i = 1; num1 / 10 != 0; i++)
+		num1 /= 10;
+
+	number = malloc(sizeof(char) * (i + 1));
+
+	for (j = 0; j < i; j++)
+	{
+		dig = num % 10;
+		number[(i - 1) - j] = dig + '0';
+		num /= 10;
+	}
+	number[j] = '\0';
+	cant = write(1, number, i);
+	free(number);
+	return (cant);
+}
